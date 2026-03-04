b3bde43d76c57c14b44d389e5cbd58b07f22bb79
charan794 <settipallicharan6@gmail.com>
2026-03-04 14:16:11 +0530

    Day 6

--- FrontendReact/src/components/Parent.jsx
+++ FrontendReact/src/components/Parent.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Child from "./Child"; // adjust path if needed
+import { userContext } from "../App";
 
 const Parent = () => {
+
+  const { name } = useContext(userContext);
+
   const getData = (data) => {
     console.log("Received from child:", data);
     alert("Received from child: " + data);
@@ -9,7 +13,10 @@ const Parent = () => {
 
   return (
     <div>
+      <h1>From App.jsx: {name}</h1>
+
       <h2>Parent Component</h2>
+
       <Child send={getData} />
     </div>
   );
--- FrontendReact/src/pages/Home.jsx
+++ FrontendReact/src/pages/Home.jsx
@@ -3,18 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        {/* <h2>Other Components</h2>
-
-      <Conditional />
-
-      <Form />
-      <State />
-      <Parent />
-      <User
-        name="xyz"
-        age={33}
-        skills={["HTML", "CSS", "JS", "JAVA"]}
-      />*/}
+        
 
     </div>
   )
