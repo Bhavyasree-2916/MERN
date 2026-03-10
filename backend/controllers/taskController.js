d1c236fc7a9574c107b33b6d1c5e0aa59c2991bd
Bhavyasree-2916 <jbhavyasree7709@gmail.com>
2026-03-10 13:19:42 +0530

    backend

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,4 @@
-const Task = require("../models/task");
+const Task = require("../models/Task");
 
 // Create a new task
 exports.createTask = async (req, res) => {
@@ -17,8 +17,75 @@ exports.createTask = async (req, res) => {
 
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      msg: "Server error"
-    });
+    res.status(500).json({ msg: "Server error" });
   }
 };
+
+// Get all tasks
+exports.getAllTask = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+
+    res.json(tasks);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.user.id
+    })
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+    res.json(task);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      {
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+      },
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+
+    res.json(task);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user.id
+    })
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+    res.json({ msg: "Task deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+}
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask } = require("../controllers/taskController");
+const { createTask,getAllTask,getTaskById,updateTask,deleteTask } = require("../controllers/taskController");
 const { protect } = require("../middleware/authMiddleware");
 
 // Protect routes
 router.use(protect);
 
 // Create task
-router.post("/create", createTask);
-
+router.post("/create",protect,createTask);
+router.get("/getTask",protect,getAllTask)
+router.get("/getTask/:id",protect,getTaskById)
+router.put("/updateTask/:id",protect,updateTask)
+router.delete("/deleteTask/:id",protect,deleteTask)
 module.exports = router;
